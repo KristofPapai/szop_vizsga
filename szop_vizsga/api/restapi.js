c37758218eb973b4d9be8da87f9f46d5dd32460b
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-01-05 16:03:11 +0100

    openapi update

--- szop_vizsga/api/restapi.js
+++ szop_vizsga/api/restapi.js
@@ -3,6 +3,33 @@ var express = require('express');
 var app = express();
 var mysql      = require('mysql');
 var bodyParser = require('body-parser');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ?? 3000;
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "MotoGP API",
+      version: "1.0.0",
+      description: "The documentation of the NodeJS API",
+    },
+    servers: [
+      {
+        url: `http://${host}:${port}`,
+      },
+    ],
+  },
+  apis: ["./*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 
 var connection = mysql.createConnection({
@@ -31,6 +58,87 @@ var server = app.listen(3000,  "127.0.0.1", function () {
 
 });
 
+/*
+@swagger
+components:
+  securitySchemes:
+    BasicAuth:
+      type: http
+      scheme: basic
+  schemas:
+    Results:
+      type: object
+      required:
+        - raceName
+        - raceWinner
+        - raceConstructor
+      properties:
+        id:
+          type: integer
+          description: The auto-generated id of the event
+        raceName:
+          type: string
+          description: The name of the event
+        raceWinner:
+          type: string
+          description: The winner of the event
+        raceConstructor:
+          type: string
+          description: The winners team of the event
+      example:
+        id: 2
+        name: Tissot Grand Prix of Doha
+        salary: Fabio Quartararo
+        age: Monster Energy Yamaha MotoGP
+  Users:
+    type: object
+    required:
+      - userName
+      - userPassword
+    properties:
+      id:
+        type: integer
+        description: The auto-generated id of the event
+      userName:
+        type: string
+        description: The username of the user
+      userPassword:
+        type: string
+        description: The password of the user
+    example:
+      id: 1
+      userName: admin
+      userPassword: admin
+*/
+
+
+
+
+/*
+@swagger
+/motogp:
+    get:
+      description: Returns all The main events of the MotoGP calendar in the database
+      responses:
+        '200':
+          description: Successfully returned a list of Events
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/Results'
+        '400':
+          description: Invalid request
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  message:
+                    type: string
+*/
+
 app.get('/motogp', function (req, res) {
   console.log("belép")
    connection.query('select * from results', function (error, results, fields) {
@@ -41,6 +149,37 @@ app.get('/motogp', function (req, res) {
 
 	});
 });
+
+/*
+@swagger
+/motogp/:login:
+    post:
+      description: POST method for login
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/Users'
+      responses:
+        '200':
+          description: Successfully returned a user
+          content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: '#/components/schemas/Users'
+        '400':
+          description: Invalid request
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  message:
+                    type: string
+*/
 
 app.post('/motogp/:login', function(request, response) {
 	var username = request.body.usernameCode;
@@ -68,6 +207,32 @@ app.post('/motogp/:login', function(request, response) {
   	}
 });
 
+/*
+@swagger
+/motogp
+  post:
+    description: Adds a new Event into the calendar
+    requestBody:
+      required: true
+      content:
+        application/json:
+          schema:
+            $ref: '#/components/schemas/Results'
+    responses:
+      '200':
+        description: Successfully created a new event
+      '400':
+        description: Invalid request
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+*/
+
+
 app.post('/motogp', function (req, res) {
    var postData  = req.body;
    connection.query('INSERT INTO results SET ?', postData, function (error, results, fields) {
@@ -75,6 +240,35 @@ app.post('/motogp', function (req, res) {
 	  res.end(JSON.stringify(results));
 	});
 });
+
+
+/*
+@swagger
+/motogp
+  put:
+    description: Updates the list of events in the SQL database
+    requestBody:
+      required: true
+      content:
+        application/json:
+          schema:
+              type: integer
+              description: Numeric ID of the event to update.
+              example:
+                id: 2
+    responses:
+      '200':
+        description: Successfully created a new event
+      '400':
+        description: Invalid request
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+*/
 
 app.put('/motogp', function (req, res) {
    console.log("belép2")
@@ -94,12 +288,25 @@ app.put('/motogp', function (req, res) {
 	});
 });
 
-app.get('/motogp/:auth', function(req, res){
-    console.log("belép az authba")
-     if(req.loggedIn){
-       console.log("good auth")
-       res.send('client_logged_in')}
-});
+/*
+@swagger
+/motogp
+  delete:
+    description: Delete an employee by id
+    responses:
+      '200':
+        description: Successfully removed the event
+      '400':
+        description: Invalid request
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                message:
+                  type: string
+*/
+
 
 app.delete('/motogp', function (req, res) {
    console.log('belép delete')
